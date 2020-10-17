bcc8a775d956f38a064e3e8363552b5e964d5646
Ajith-Rahul <asanaboina@deakin.edu.au>
2020-10-17 17:00:56 +1100

    does

---
+++ public/env.js
@@ -0,0 +1,13 @@
+console.log('hello world')
+$(document).ready(function(){
+    console.log('d ready')
+    $('#calculateBtn').click(function(){
+        var number1=$('#number1').val()
+        var number2=$('#number2').val()
+        var numbers={
+            num1:number1,
+            num2:number2
+        }
+
+    })
+})
