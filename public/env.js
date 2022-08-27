6d1b7fe7038d676794908716e9ec454f6550bb26
Ajith-Rahul <68560635+Ajith-Rahul@users.noreply.github.com>
2022-08-27 22:55:27 +1000

    Update env.js

--- public/env.js
+++ public/env.js
@@ -1,12 +1,10 @@
-console.log('hello world')
+
 $(document).ready(function(){
-    console.log('d ready')
     $('#calculateBtn').click(function(){
         var number1=parseInt($('#number1').val())
         var number2=parseInt($('#number2').val())
         var results=number1+number2;
         $('#result').val(results)
-        console.log('gf',results)
        
     })
 })
